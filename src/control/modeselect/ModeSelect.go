be27e9897eb60b4abeec40617f54a3ebb0d1b8f4
Mohamed Alzahmi <mohamedalzahmi@Mohameds-MacBook-Air.local>
2020-04-21 16:53:36 +0400

    initial commit

--- src/control/modeselect/ModeSelect.go
+++ src/control/modeselect/ModeSelect.go
@@ -1,5 +1,7 @@
 package modeselect
 
+import "fmt"
+
 // VentMode is a custom type struct to identify the mode
 // of ventilation required
 type VentMode struct {
@@ -9,6 +11,16 @@ type VentMode struct {
 
 // ReadFromGUI reads input from the GUI to select the
 // required Mode
-func (M *VentMode) ReadFromGUI() {
-
+func (M *VentMode) ReadFromGUI(mode string) {
+	switch mode {
+	case "AC":
+		fmt.Println("Assisted Control Mode selected")
+	case "VC":
+		fmt.Println("Volume Control Mode selected")
+	case "PC":
+		fmt.Println("Pressure Control Mode selected")
+	default:
+		fmt.Println("No Ventilator Mode selected")
+		return
+	}
 }
