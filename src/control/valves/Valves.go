4be2f0157309a66dd3dd2fd2883331c7bf4e6d93
Mohamed Alzahmi <mohamedalzahmi@Mohameds-MacBook-Air.local>
2020-04-21 16:28:34 +0400

    initial commit

--- src/control/valves/Valves.go
+++ src/control/valves/Valves.go
@@ -2,36 +2,41 @@ package valves
 
 import "fmt"
 
-//state is true if valve is open
+//SolenValve is a custom type struct which contains
+//Solenoid ID, Address and State
 type SolenValve struct {
 	Address int
 	ID      string
 	state   bool
 }
 
+//PropValve is a custom type struct which contains
+//proportional valve ID, Address and Percentage
 type PropValve struct {
 	Address int
 	ID      string
 	Percent int
 }
 
+//SolenCmd switchs on and off the solenoids
 func (valve *SolenValve) SolenCmd(cmd string) {
 
 	switch cmd {
 	case "open":
 		valve.state = true
-		fmt.Println("Valve (%v)", valve.ID, " has opened")
+		fmt.Printf("Valve (%v) has opened\n", valve.ID)
 	case "close":
 		valve.state = false
-		fmt.Println("Valve (%v)", valve.ID, " has opened")
+		fmt.Printf("Valve (%v) has closed\n", valve.ID)
 
 	}
 
 }
 
+//IncrementValve adjusts the proportionality of the proportional valve
 func (valve *PropValve) IncrementValve(percent int) {
 
 	valve.Percent = percent
-	fmt.Println("Valve (%v)", valve.ID, " opening has been set to %v", valve.ID)
+	fmt.Printf("Valve (%v) opening has been set to %v\n", valve.ID, valve.Percent)
 
 }
