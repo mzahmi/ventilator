0a3e75123ad10b993cae350547bcc9015c6a7c68
Mohamed Alzahmi <mohamedalzahmi@Mohameds-MacBook-Air.local>
2020-04-26 12:55:23 +0400

    third dev commit

--- control/modeselect/ModeSelect.go
+++ control/modeselect/ModeSelect.go
@@ -2,116 +2,80 @@ package modeselect
 
 import (
 	"fmt"
+	"github.com/mzahmi/ventilator/control/valves"
 	"github.com/mzahmi/ventilator/control/sensors"
-	"time"
 )
 
-// VentMode is a custom type struct to identify the mode
-// of ventilation required
-type VentMode struct {
-	ID   int
-	Mode string
+// UserInput is a custome type struct that contains the global
+// variables input by the user or operator
+type UserInput struct {
+	Mode                string
+	BreathType          string
+	PatientTriggerType  string
+	TidalVolume         float32 // ml
+	Rate                float32 // BPM
+	Ti                  float32 // inhalation time
+	IR                  float32 // inhalation ratio part
+	ER                  float32 // exhalation ratio part
+	PeakFlow            float64
+	PEEP                float32 // 5-20 mmH2O
+	FiO2                float32 // 21% - 100%
+	PressureTrigSense   float32 // -0.5 to 02 mmH2O
+	FlowTrigSense       float32 // 0.5 to 5 Lpm
+	PressureSupport     float32 // needs to be defined
+	InspiratoryPressure float32
+	PressureControl     float32
 }
 
-// VolumeAC is a custom type struct that defines the required
-// variables for Volume Assist Control ventilation mode
-// Trigger is chosen, cycling is time, controlling is volume
-type VolumeAC struct {
-	TidalVolume float64
-	Rate        float64
-	Ti          float64
-	IR          float64
-	ER          float64
-	PeakFlow    float64
-	PEEP        float64
-	FiO2        int
-	Trigger     string
-	TrigSense   float64
-}
-
-// Triggering is a method set for Volume AC mode which returns
-// trigger value based three options chosen by operator
-// default is Time triggering returns 0
-func (VAC *VolumeAC) Triggering() float64 {
-	switch VAC.Trigger {
-	case "Pressure":
-		return VAC.PEEP + VAC.TrigSense
-	case "Flow":
-		return VAC.TrigSense
-	default:
-		return 0
-	}
-}
-
-// Cycling is a method set for Volume AC mode which returns
-// the exhalation time Te
-func (VAC *VolumeAC) Cycling() float64 {
-	var BCT float64 = 60.0 / VAC.Rate
-	switch {
-	case VAC.Ti != 0:
-		return BCT - VAC.Ti
-	case VAC.IR != 0:
-		VAC.Ti = BCT * VAC.IR / (VAC.IR + VAC.ER)
-		return BCT - VAC.Ti
-	case VAC.PeakFlow != 0:
-		VAC.Ti = VAC.TidalVolume / VAC.PeakFlow
-		return BCT - VAC.Ti
-	default:
-		return BCT - VAC.Ti
-	}
-}
-
-// Control is a method set for Volume AC mode which controls the vent
-func (VAC *VolumeAC) Control() {
-	if VAC.Ti != 0 {
-		VAC.PeakFlow = VAC.TidalVolume / VAC.Ti
-	} else {
-		VAC.Ti = VAC.TidalVolume / VAC.PeakFlow
-	}
-	trig := VAC.Triggering()
-	te := VAC.Cycling()
-	ti := VAC.Ti
-	P1 := sensors.Pressure{
-		ID:       1,
-		Address:  0,
-		RawValue: 255,
-		MMH2O:    3,
-	}
-
-	go func() {
-		for {
-			//Turn on the inhalation from reading F1 or P1
-			if trig == 0 {
-				fmt.Println("Inhale on time trig")
-				time.Sleep(time.Second * time.Duration(ti))
-				fmt.Println("Exhale on time trig")
-				time.Sleep(time.Second * time.Duration(te))
-			} else if trig < 0 {
-				//reading from P1 trig on calculated value
-				if P1.MMH2O == trig {
-					fmt.Println("Inhael on P trig")
-				}
-
-				fmt.Println("Exhale on P trig")
-			}
-
-		}
-	}()
-
-}
-
-// ReadFromGUI reads input from the GUI to select the
+// ModeSelection reads input from the GUI to select the
 // required Mode
-func (M *VentMode) ReadFromGUI(mode string) {
-	switch mode {
+func ModeSelection(UI *UserInput) {
+	switch UI.Mode {
 	case "Volume A/C":
 		fmt.Println("Volume Assisted Control Mode selected")
-	case "VC":
-		fmt.Println("Volume Control Mode selected")
-	case "PC":
-		fmt.Println("Pressure Control Mode selected")
+		VolumeAC(UI)
+	case "Pressure A/C":
+		fmt.Println("Pressure Assisted Control Mode selected")
+	case "PSV":
+		fmt.Println("Pressure Support Control Mode selected")
+	case "V-SMIV":
+		fmt.Println("Volume SIMV Mode selected")
+	case "P-SIMV":
+		fmt.Println("Pressure SIMV Mode selected")
 	default:
 		fmt.Println("No Ventilator Mode selected")
 		return
 	}
+}
+
+// VolumeAC ...
+func VolumeAC(UI *UserInput) {
+	switch UI.BreathType {
+	case "control":
+		VolumeControl(UI)
+	case "assist":
+		VolumeAssist(UI)
+	default:
+		fmt.Println("Enter valid breath type")
+	}
+}
+
+// VolumeControl ...
+func VolumeControl(UI *UserInput) {
+	FlowPID := NewPIDController(0.5, 0.5, 0.5)
+	FlowPID = FlowPID.Set(UI.PeakFlow)
+	F1 := valves.PropValve{1,"flow actuator",0}
+	Fsense := sensors.Flow{1,1,1,1}
+	for start:=time.Now(); time.Since(start) < (UI.Ti * time.Second) {
+		F1.IncrementValve(FlowPID.Update(Fsense.ReadFlow()))
+
+	}
+	F1.IncrementValve(0) // closes the valve
+	
+
+}
+
+// VolumeAssist ...
+func VolumeAssist(UI *UserInput) {
+
 }
--- control/sensors/Sensors.go
+++ control/sensors/Sensors.go
@@ -11,10 +11,26 @@ type Pressure struct {
 	MMH2O float64
 }
 
+// Flow is a custom type struct to identify onboard
+// pressure sensors
+type Flow struct {
+	ID       int
+	Address  int
+	RawValue float64
+	Rate     float64
+}
+
 // ReadPressure ... reads raw data from attached sensors to
 // Memberane based on its address
 func (PS *Pressure) ReadPressure() {
 
+}
+
+// ReadFlow ... reads raw data from attached sensors to
+// Memberane based on its address
+func (PS *Flow) ReadFlow() float64 {
+	var flowRate float64
+	return flowRate
 }
 
 // ConvertRawPressure ... converts the raw data to pressure units
