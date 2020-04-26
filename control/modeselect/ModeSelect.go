d7e0a32d1f3cedcf8ca62330e3a7cbc03fba58f8
Mohamed Alzahmi <mohamedalzahmi@Mohameds-MacBook-Air.local>
2020-04-26 20:57:22 +0400

    forth dev commit

--- control/modeselect/ModeSelect.go
+++ control/modeselect/ModeSelect.go
@@ -2,8 +2,9 @@ package modeselect
 
 import (
 	"fmt"
-	"github.com/mzahmi/ventilator/control/valves"
 	"github.com/mzahmi/ventilator/control/sensors"
+	"github.com/mzahmi/ventilator/control/valves"
+	"time"
 )
 
 // UserInput is a custome type struct that contains the global
@@ -62,16 +63,37 @@ func VolumeAC(UI *UserInput) {
 
 // VolumeControl ...
 func VolumeControl(UI *UserInput) {
-	FlowPID := NewPIDController(0.5, 0.5, 0.5)
-	FlowPID = FlowPID.Set(UI.PeakFlow)
-	F1 := valves.PropValve{1,"flow actuator",0}
-	Fsense := sensors.Flow{1,1,1,1}
-	for start:=time.Now(); time.Since(start) < (UI.Ti * time.Second) {
-		F1.IncrementValve(FlowPID.Update(Fsense.ReadFlow()))
+	//calculate Te from UI.Ti and BCT
+	BCT := 60 / UI.Rate
+	Te := BCT - UI.Ti
+	//initiate a PID controller based on the PeakFlow
+	FlowPID := NewPIDController(0.5, 0.5, 0.5) // takes in P, I, and D values
+	FlowPID = FlowPID.Set(UI.PeakFlow)         // Sets the PID setpoint
+
+	// Identify the main valves or solenoids by MIns and MExp
+	MIns := valves.PropValve{ID: "A_PSV_INS", Address: "GPIO01", Percent: 0}   //normally closed
+	MExp := valves.PropValve{ID: "A_PSV_EXP", Address: "GPIO02", Percent: 100} //normally open
+
+	// Identify the flow sensors by FIns and FExp
+	FIns := sensors.Flow{ID: "SNS_F_INS", Address: "GPIO03", RawValue: 0, Rate: 0}
+	FExp := sensors.Flow{ID: "SNS_F_EXP", Address: "GPIO04", RawValue: 0, Rate: 0}
+
+	//control loop
+	for {
+		//Open main valve MIns controlled by flow sensor FIns
+		for start := time.Now(); time.Since(start) < (time.Duration(UI.Ti) * time.Second); {
+			MIns.IncrementValve(FlowPID.Update(FIns.ReadFlow()))
+		}
+		//Close main valve MIns
+		MIns.IncrementValve(0) // closes the valve
+		//Open main valve MExp controlled by flow sensor FExp
+		for start := time.Now(); time.Since(start) < (time.Duration(Te) * time.Second); {
+			MExp.IncrementValve(FlowPID.Update(FExp.ReadFlow()))
+		}
+		//Close main valve MExp
+		MExp.IncrementValve(0) // closes the valve
 
 	}
-	F1.IncrementValve(0) // closes the valve
-	
 
 }
 
--- control/sensors/Sensors.go
+++ control/sensors/Sensors.go
@@ -3,43 +3,33 @@ package sensors
 // Pressure is a custom type struct to identify onboard
 // pressure sensors
 type Pressure struct {
-	ID       int
-	Address  int
+	ID       string
+	Address  string
 	RawValue float64
-	//Pascal   float64
-	//Bar      float64
-	MMH2O float64
+	MMH2O    float64
 }
 
 // Flow is a custom type struct to identify onboard
-// pressure sensors
+// flow sensors
 type Flow struct {
-	ID       int
-	Address  int
+	ID       string
+	Address  string
 	RawValue float64
 	Rate     float64
 }
 
 // ReadPressure ... reads raw data from attached sensors to
 // Memberane based on its address
-func (PS *Pressure) ReadPressure() {
+func (PS *Pressure) ReadPressure() float64 {
+	//read raw data from source and convert to mmH2O pressure reading
+
+	return PS.MMH2O
 
 }
 
 // ReadFlow ... reads raw data from attached sensors to
 // Memberane based on its address
 func (PS *Flow) ReadFlow() float64 {
-	var flowRate float64
-	return flowRate
-}
-
-// ConvertRawPressure ... converts the raw data to pressure units
-// returns Pascal, Bar, mmH2O
-func (PS *Pressure) ConvertRawPressure() {
-
-}
-
-// SendToGUI ... sends processed pressure sensor data to GUI
-func (PS *Pressure) SendToGUI() {
-
+	//read raw data from source and convert to flow rate reading
+	return PS.Rate
 }
