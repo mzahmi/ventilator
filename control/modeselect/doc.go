fc6ca1d945ecd79d74f88b8fd74aa13d6749debe
rashed <x.suwaidi@gmail.com>
2020-04-30 09:44:55 +0400

    docs

--- control/modeselect/doc.go
+++ control/modeselect/doc.go
@@ -1,8 +1,1 @@
-/*
-Package modeselect decides which mode should be used for the ventilator and what related functions whould be
-activated depending on the GUI input. There are 5 main modes:
-
-	1.	Volume AC
-	2.	Volume pressue
-*/
 package modeselect
