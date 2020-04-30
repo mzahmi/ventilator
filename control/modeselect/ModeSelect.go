6c0f13906e442f566c5c501ecd2724104630edbd
Mohamed Alzahmi <mohamedalzahmi@Mohameds-MacBook-Air.local>
2020-04-30 08:59:01 +0400

    started on PSV mode

--- control/modeselect/ModeSelect.go
+++ control/modeselect/ModeSelect.go
@@ -18,6 +18,7 @@ type UserInput struct {
 	TidalVolume         float32 // ml
 	Rate                float32 // BPM
 	Ti                  float32 // inhalation time
+	TiMax               float32 // for PSV mode backup time control
 	Te                  float32 // exhalation time
 	IR                  float32 // inhalation ratio part
 	ER                  float32 // exhalation ratio part
@@ -26,6 +27,7 @@ type UserInput struct {
 	FiO2                float32 // 21% - 100%
 	PressureTrigSense   float32 // -0.5 to 02 mmH2O
 	FlowTrigSense       float32 // 0.5 to 5 Lpm
+	FlowCyclePercent    float32 // for flow cycling ranges from 0 to 100%
 	PressureSupport     float32 // needs to be defined
 	InspiratoryPressure float32
 	PressureControl     float32
@@ -305,4 +307,87 @@ func PressureAssist(UI *UserInput) {
 			}
 		}
 	}
+}
+
+// PSV Pressure support ventilation mode
+func PSV(UI *UserInput) {
+	UpdateValues(UI)
+	//Initialize  Sensors at inhalation side
+	PIns := sensors.Pressure{Name: "SNS_P_INS", ID: 0, AdcID: 1, MMH2O: 0} //insparatory pressure sensor
+	FIns := sensors.Flow{Name: "SNS_F_INS", ID: 1, AdcID: 1, Rate: 0}
+	//Initialize  Sensors at exhalation side
+	PExp := sensors.Pressure{Name: "SNS_P_EXP", ID: 2, AdcID: 1, MMH2O: 0} //expratory pressure sensor
+	//FExp := sensors.Flow{Name: "SNS_F_EXP", ID: 3, AdcID: 1, Rate: 0}
+	//Initialize valves
+	MIns := valves.PropValve{Name: "A_PSV_INS", DacChan: 0, DacID: 1, Percent: 0} //normally closed
+	MExp := valves.PropValve{Name: "A_PSV_EXP", DacChan: 1, DacID: 1, Percent: 0} //normally open
+	//Initialize PID controller
+	//initiate a PID controller based on the PeakFlow
+	PressurePID := NewPIDController(0.5, 0.5, 0.5) // takes in P, I, and D values
+
+	//Check trigger type
+	switch UI.PatientTriggerType {
+	case "Pressure":
+		//Calculate trigger threshhold with PEEP and sensitivity
+		PTrigger := UI.PEEP + UI.PressureTrigSense
+
+		counter := 0
+		//Begin loop
+		for !Exit {
+			FlowMonitor := []float32{FIns.ReadFlow()}
+			//check if trigger is true
+			if PIns.ReadPressure() <= PTrigger {
+				PressurePID.setpoint = float64(UI.InspiratoryPressure) // Sets PID setpoint to Inspiratory pressure
+				//Open main valve MIns controlled by flow sensor FIns
+				for start := time.Now(); time.Since(start) < (time.Duration(UI.TiMax*1000) * time.Millisecond); {
+					MIns.IncrementValve(PressurePID.Update(float64(PIns.ReadPressure())))
+					if FlowMonitor[counter] < FIns.ReadFlow() {
+						FlowMonitor = append(FlowMonitor, FIns.ReadFlow())
+					} else if FlowMonitor[counter] > FIns.ReadFlow() {
+						FlowMax := FlowMonitor[counter]
+					}
+					counter++
+				}
+				//Close main valve MIns
+				MIns.IncrementValve(0) // closes the valve
+
+				//Open main valve MExp controlled by flow sensor FExp
+				for start := time.Now(); time.Since(start) < (time.Duration(UI.Te*1000) * time.Millisecond); {
+					if PExp.ReadPressure() <= UI.PEEP {
+						break
+					}
+					MExp.IncrementValve(100)
+				}
+				//Close main valve MExp
+				MExp.IncrementValve(0) // closes the valve
+			}
+		}
+	case "Flow":
+		//Calculate trigger threshhold with flow trig sensitivity
+		FTrigger := UI.FlowTrigSense
+		//Begin loop
+		for !Exit {
+			//check if trigger is true
+			if FIns.ReadFlow() >= FTrigger {
+				PressurePID.setpoint = float64(UI.InspiratoryPressure) // Sets PID setpoint to Inspiratory pressure
+				//Open main valve MIns controlled by flow sensor FIns
+				for start := time.Now(); time.Since(start) < (time.Duration(UI.Ti*1000) * time.Millisecond); {
+					MIns.IncrementValve(PressurePID.Update(float64(PIns.ReadPressure())))
+				}
+				//Close main valve MIns
+				MIns.IncrementValve(0) // closes the valve
+
+				//Open main valve MExp controlled by flow sensor FExp
+				for start := time.Now(); time.Since(start) < (time.Duration(UI.Te*1000) * time.Millisecond); {
+					if PExp.ReadPressure() <= UI.PEEP {
+						break
+					}
+					MExp.IncrementValve(100)
+				}
+				//Close main valve MExp
+				MExp.IncrementValve(0) // closes the valve
+			}
+		}
+	}
+
 }
