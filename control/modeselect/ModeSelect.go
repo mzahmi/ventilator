67e67c1c9efc55ca7858e7e9481b7c1dd910b601
rashed <x.suwaidi@gmail.com>
2020-04-30 10:06:54 +0400

    docs

--- control/modeselect/ModeSelect.go
+++ control/modeselect/ModeSelect.go
@@ -1,12 +1,3 @@
-/*
-Package modeselect decides which mode should be used for the ventilator and what related functions whould be
-activated depending on the GUI input. There are 5 main modes:
-
-	1.	Volume AC
-	2.	Pressure AC
-	3.	PSV
-	4.	SMV
-*/
 package modeselect
 
 import (
--- control/modeselect/doc.go
+++ control/modeselect/doc.go
@@ -1,1 +1,12 @@
+/*
+Package modeselect decides which mode should be used for the ventilator and what related functions whould be
+activated depending on the GUI input. There are 5 main modes:
+
+	1.	Volume AC
+	2.	Pressure AC
+	3.	PSV
+	4.	SMV
+
+For more information look at https://oxfordmedicine.com/view/10.1093/med/9780198784975.001.0001/med-9780198784975-chapter-8.
+*/
 package modeselect
