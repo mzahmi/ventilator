feced619a921f0374577ccd88c9cd8858e7cbc4d
Hamad Binghalib <hamadbinghalib@gmail.com>
2020-05-05 13:57:00 +0400

    added hadware initialization

---
+++ control/init/hardwareInit.go
@@ -0,0 +1,143 @@
+package init
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/mzahmi/ventilator/control/adc"
+	"github.com/mzahmi/ventilator/control/dac"
+	"github.com/mzahmi/ventilator/control/ioexp"
+)
+
+//InitHardware ... this function should be called at the beginning of main.
+//It will initialize all the hardware and check for errors
+func hardwareInit() {
+	fmt.Println("Beginning hardware initialization")
+
+	//I2C init
+	initI2C()
+	//init ADC
+	initADC()
+	//init DAC
+	initDAC()
+
+	fmt.Println("End of hardware initialization")
+
+}
+
+//initI2C ... this function initializes I2C and checks for errors
+func initI2C() {
+	fmt.Println("Starting I2C init")
+	err := ioexp.InitChip()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//Beep test
+	fmt.Println("beep called")
+	err := rpigpio.BeepOn()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	time.Sleep(2000 * time.Millisecond)
+	err = rpigpio.BeepOff()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//testing solenoids
+	// err := ioexp.WritePin(solPinMask, true)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// time.Sleep(time.Duration(onTime) * time.Second)
+	// err = ioexp.WritePin(solPinMask, false)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	//testing LEDs
+	const blinkTime = 500
+	fmt.Println("Blinking LEDs")
+	for ii := 0; ii < 10; ii++ {
+
+		fmt.Println("Yellow")
+		err := ioexp.WritePin(ioexp.YellowLed, true)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		time.Sleep(blinkTime * time.Millisecond)
+		err = ioexp.WritePin(ioexp.YellowLed, false)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println("Red")
+		err = ioexp.WritePin(ioexp.RedLed, true)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		time.Sleep(blinkTime * time.Millisecond)
+		err = ioexp.WritePin(ioexp.RedLed, false)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println("Green")
+		err = ioexp.WritePin(ioexp.GreenLed, true)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		time.Sleep(blinkTime * time.Millisecond)
+		err = ioexp.WritePin(ioexp.GreenLed, false)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println("Blue")
+		err = ioexp.WritePin(ioexp.BlueLed, true)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		time.Sleep(blinkTime * time.Millisecond)
+		err = ioexp.WritePin(ioexp.BlueLed, false)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+}
+
+//initADC ... this function initializes ADC and checks for errors
+func initADC() {
+
+	fmt.Println("readAdc called")
+
+	adcReadResults, err := adc.ReadADC(1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
+
+//initDAC ... this function initializes DAC and checks for errors
+func initDAC() {
+
+	fmt.Println("dacsZero called")
+	err := dac.DacsAllZeroOut()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
