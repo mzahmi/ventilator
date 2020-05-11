90b4fc7800990396e6fcefc378971a9335dc7e4f
Tarek Taha <tarek.taha@gmail.com>
2020-05-11 22:24:28 +0400

    -removed old gui

--- gui/qml/ViewModeSelector.js
+++
@@ -1,8 +0,0 @@
-var modes = ["Pressure AC", "Volume AC","PSV", "P-SIMV", "V-SIMV"]
-var breath = [["Control", "Assist"],["Control", "Assist"],["Support"], ["Control", "Assist", "Support"], ["Control", "Assist", "Support"]]
-
-/*
-  modes =   ["Volume AC", "Pressure AC","PSV", "P-SIMV", "V-SIMV"]
-  Breathe = ["Control", "Assist", "Support"]
-  trigger = ["pressure", "volume"]
-*/
--- gui/qml/ViewSettings.qml
+++
@@ -1,9 +0,0 @@
-import QtQuick 2.0
-
-Item {
-    anchors.fill: parent
-    Rectangle{
-        anchors.fill: parent
-    }
-
-}
