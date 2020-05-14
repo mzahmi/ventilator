75dec7f56df60d9d33c9149f6bf23ccedfd9a59d
Rashed Suwaidi <x.suwaidi@gmail.com>
2020-05-14 10:34:14 +0400

    word wrap text

--- gui/redis_manager.py
+++ gui/redis_manager.py
@@ -2,9 +2,14 @@ import config
 
 
 # r.mset({"alarm_status": "j"})
-config.r.mset({"alarm_info": "none"})
+config.r.mset({"alarm_title": "this title"})
+config.r.mset({"alarm_status": "none"})
+config.r.mset(
+    {"alarm_text": "this text is very long and will take up more than the allocated space"})
 # r.mset({"alarm_title": "none"})
 
 # print(r.get("alarm_title"))
-print(config.r.get("alarm_info"))
+print(config.r.get("alarm_title"))
+print(config.r.get("alarm_text"))
+print(config.r.get("alarm_status"))
 # print(r.get("alarm_text"))
