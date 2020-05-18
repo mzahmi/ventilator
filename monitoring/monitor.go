8511ae53105a7ca3b3b0393aa4df3e4b837d7c29
mzahmi <63805103+mzahmi@users.noreply.github.com>
2020-05-18 21:28:53 +0400

    monitoring package added

---
+++ monitoring/monitor.go
@@ -0,0 +1,17 @@
+package monitoring
+
+func MonitorRR() {
+	//TODO: find a way to properly monitor the resparatory rate initiated by patient
+
+}
+func MonitorMV() {
+	//TODO: find a way to properly monitor the minute volume
+}
+
+func MonitorFlow() {
+	//TODO: find a way to properly monitor the flow rate of tidal volume
+}
+
+func MonitorPIP() {
+	//TODO: find a way to properly monitor the peak inspratory preasure
+}
