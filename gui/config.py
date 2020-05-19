84251da6af420c4f55d361a09b311c2a5ea51ae9
Rashed Suwaidi <x.suwaidi@gmail.com>
2020-05-19 16:43:07 +0400

    dynamic default values

--- gui/config.py
+++ gui/config.py
@@ -1,8 +1,10 @@
 import redis
 import logging
 
+
 logging.basicConfig(filename='log.log', format='%(levelname)s:%(asctime)s - %(message)s',
                     datefmt='%H:%M:%S', level=logging.NOTSET)
+
 useredis = False
 r = redis.StrictRedis(
     host='dupi1.local',
